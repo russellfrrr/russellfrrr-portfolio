741f2eb273dcfe8cb1492ac08399b942abfc9659
Russell Ferrero <russellferrero00@gmail.com>
2026-01-25 22:14:01 +0800

    refactor: change emailjs vars to .env file

--- p2-web-dev-portfolio-new/src/config/emailjs.js
+++ p2-web-dev-portfolio-new/src/config/emailjs.js
@@ -1,12 +1,16 @@
 import emailjs from '@emailjs/browser';
 
-emailjs.init('XhQeCvEOTfzUU0krZ');
+emailjs.init(import.meta.env.VITE_EMAILJS_PUBLIC_KEY);
 
 export const sendEmail = async (formData) => {
-  return emailjs.send('service_qmrtn7u', 'template_lcxhib4', {
-    to_email: 'russellferrero@gmail.com',
-    from_name: formData.name,
-    from_email: formData.email,
-    message: formData.message
-  });
+  return emailjs.send(
+    import.meta.env.VITE_EMAILJS_SERVICE_ID,
+    import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+    {
+      to_email: 'russellferrero@gmail.com',
+      from_name: formData.name,
+      from_email: formData.email,
+      message: formData.message
+    }
+  );
 };
