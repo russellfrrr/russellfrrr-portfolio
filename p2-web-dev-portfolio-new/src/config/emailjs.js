06ba0eed6b3b9cd812c00dd58dac67966c25ec05
Russell Ferrero <russellferrero00@gmail.com>
2026-01-25 22:31:03 +0800

    fix: add emailjs bug test

--- p2-web-dev-portfolio-new/src/config/emailjs.js
+++ p2-web-dev-portfolio-new/src/config/emailjs.js
@@ -1,16 +1,20 @@
 import emailjs from '@emailjs/browser';
 
-emailjs.init(import.meta.env.VITE_EMAILJS_PUBLIC_KEY);
-
 export const sendEmail = async (formData) => {
+  console.log('ENV CHECK:', {
+    publicKey: import.meta.env.VITE_EMAILJS_PUBLIC_KEY,
+    serviceId: import.meta.env.VITE_EMAILJS_SERVICE_ID,
+    templateId: import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+  });
+
   return emailjs.send(
     import.meta.env.VITE_EMAILJS_SERVICE_ID,
     import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
     {
-      to_email: 'russellferrero@gmail.com',
       from_name: formData.name,
       from_email: formData.email,
-      message: formData.message
-    }
+      message: formData.message,
+    },
+    import.meta.env.VITE_EMAILJS_PUBLIC_KEY
   );
 };
