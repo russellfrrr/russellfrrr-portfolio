3fd8600f3b7216729cd9382ae603be913f308e02
Russell Ferrero <russellferrero00@gmail.com>
2026-01-25 22:37:24 +0800

    refactor: remove key logger on emailjs bug test

--- p2-web-dev-portfolio-new/src/config/emailjs.js
+++ p2-web-dev-portfolio-new/src/config/emailjs.js
@@ -1,12 +1,6 @@
 import emailjs from '@emailjs/browser';
 
 export const sendEmail = async (formData) => {
-  console.log('ENV CHECK:', {
-    publicKey: import.meta.env.VITE_EMAILJS_PUBLIC_KEY,
-    serviceId: import.meta.env.VITE_EMAILJS_SERVICE_ID,
-    templateId: import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
-  });
-
   return emailjs.send(
     import.meta.env.VITE_EMAILJS_SERVICE_ID,
     import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
